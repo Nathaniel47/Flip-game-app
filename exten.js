861d7c1bbdcdfbec2a1072b47e927b9f4f64d810
Nathaniel47 <nathanielankomahaidoo@gmail.com>
2024-04-30 18:36:02 +0000

    added a new repository

---
+++ exten.js
@@ -0,0 +1,8 @@
+const user ={
+  name:"John",
+  age: 26,
+  email:"nathani65@gmail.com"
+
+}
+
+console.log(user.email);
---
+++ script.js
@@ -0,0 +1,5 @@
+function flipCard(cardId) {
+    var card = document.getElementById(cardId);
+    card.classList.toggle('flipped');
+  }
+  
